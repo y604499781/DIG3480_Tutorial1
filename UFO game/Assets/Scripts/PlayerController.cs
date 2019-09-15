e946437a94064432c5cc2507d68be8817432e2f6
y604499781 <54754052+y604499781@users.noreply.github.com>
2019-09-15 16:41:06 -0400

    UFO game

---
+++ UFO game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+
+    void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
+
+    void FixedUpdate ()
+    {
+        //movement of the player object with keyboard
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+
+        
+        //escape key to quit game
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    //pickups inactive when collide with player and gets a point each time
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1; //track points
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count>=12)
+        {
+            winText.text = "You Win! Game created by Yi Chen";
+        }
+    }
+}
